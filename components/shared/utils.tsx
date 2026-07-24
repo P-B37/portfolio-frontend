5b5ef40164fefa6417b55e58d86205fd554fb445
Ndeze Bonheur Emmanuel <bonheurndezenc@gmail.com>
2026-07-24 12:57:17 +0200

    Change GitHub URL in SOCIALS array
    Updated GitHub URL for the profile.

--- components/shared/utils.tsx
+++ components/shared/utils.tsx
@@ -14,7 +14,7 @@ export type Socials = z.infer<typeof socialSchema>;
 
 export const SOCIALS:Socials[] = [
   { icon: <Linkedin className="h-5 w-5 text-muted-foreground hover:text-foreground" />, label: "LinkedIn", url: "https://www.linkedin.com/in/bonheur-ndeze-bne/" },
-  { icon: <Github className="h-5 w-5 text-muted-foreground hover:text-foreground" />, label: "GitHub", url: "https://github.com/bonheurNE07" },
+  { icon: <Github className="h-5 w-5 text-muted-foreground hover:text-foreground" />, label: "GitHub", url: "https://github.com/bendeze" },
   { icon: <X className="h-5 w-5 text-muted-foreground hover:text-foreground" />, label: "Twitter", url: "https://x.com/ndeze_emmanuel" },
   { icon: <Whatsapp className="h-5 w-5 text-muted-foreground hover:text-foreground" />, label: "WhatsApp", url: "https://wa.me/250791348888" },
 ];
